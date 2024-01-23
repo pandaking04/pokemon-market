d9f5cc7379926a79f92b7c46970abb2e4652a8a4
unknown <khattiyatungklang@gmail.com>
2024-01-23 16:59:36 +0700

    second commit

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "../css/Cart.css";
-import CardInCart from "./CardInCart";
+import CardInCart from './CardInCart'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 interface CartItem {
-  id: string;
-  name: string;
-  image: string;
-  price: number;
-  stock: number;
+    id: string;
+    name: string;
+    image: string;
+    price: number;
+    stock: number;
 }
 
 interface CartProps {
@@ -20,42 +20,39 @@ interface CartProps {
     name: string;
     image: string;
     price: number;
+    
   }[];
   setCardsInCart: React.Dispatch<React.SetStateAction<CartItem[]>>;
+  
 }
 
-const Cart: React.FC<CartProps> = ({
-  isOpen,
-  onClose,
-  cardsInCart,
-  setCardsInCart,
-}) => {
-  const handleClearAll = () => {
-    setCardsInCart([]);
-  };
-  const totalItems = cardsInCart.reduce((total, card) => total + 1, 0);
-  const totalPrice = cardsInCart.reduce((total, card) => total + card.price, 0);
+const Cart: React.FC<CartProps> = ({ isOpen, onClose,cardsInCart, setCardsInCart }) => {
+
+    const handleClearAll = () => {
+        
+        setCardsInCart([]);
+      };
+      const totalItems = cardsInCart.reduce((total, card) => total + 1, 0);
+      const totalPrice = cardsInCart.reduce((total, card) => total + card.price, 0);
   return (
     <div className={`cart-container ${isOpen ? "open" : ""}`}>
       <div className="header-cart">
         <div>
           <h2>Cart</h2>
-          <button onClick={handleClearAll} className="clear-all">
-            Clear all
-          </button>
+          <button onClick={handleClearAll} className="clear-all">Clear all</button>
         </div>
 
         <button className="close-button" onClick={onClose}>
           <FontAwesomeIcon icon={faTimes} />
         </button>
       </div>
-      <div className="cart-header">
-        <p>Item</p>
-        <p>Qty</p>
-        <p>Price</p>
-      </div>
+        <div className="cart-header">
+            <p>Item</p>
+            <p>Qty</p>
+            <p>Price</p>
+        </div>
       <div className="cart-content">
-        {cardsInCart.map((card) => (
+      {cardsInCart.map((card) => (
           <CardInCart key={card.id} card={card} />
         ))}
       </div>
